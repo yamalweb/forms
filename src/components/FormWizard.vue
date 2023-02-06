74f7f3b39c705e45745785b0450c19531be57cf6
yamalweb <o.starigin@gmail.com>
2023-02-06 11:16:50 +0300

    format code

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -2,52 +2,61 @@
   <div>
     <div v-if="wizzardInProgress" v-show="asyncState !== 'pending'">
       <keep-alive>
-        <Component ref="currentStep" :is="currentStep"
-         :wizardData="form"  @updateAsyncState="updateAsyncState"/>
+        <Component
+          ref="currentStep"
+          :is="currentStep"
+          :wizardData="form"
+          @updateAsyncState="updateAsyncState"
+        />
       </keep-alive>
-  
+
       <div class="progress-bar">
         <div :style="`width: ${progress}%;`"></div>
       </div>
-  
+
       <!-- Actions -->
       <div class="buttons">
-        <button @click="goBack" v-if="currentStepNumber > 1" class="btn-outlined">Back
+        <button @click="goBack" v-if="currentStepNumber > 1" class="btn-outlined">
+          Back
         </button>
-        <button @click="nextButtonActions" class="btn">{{isLastStep?'Confirm':'Next'}}</button>
+        <button @click="nextButtonActions" class="btn">
+          {{ isLastStep ? "Confirm" : "Next" }}
+        </button>
       </div>
-  
+
       <pre><code>{{ form }}</code></pre>
     </div>
     <div v-else>
       <h1 class="title">343234 234234</h1>
-      <h2 class="subtitle">
-        Awesome!
-      </h2>
+      <h2 class="subtitle">Awesome!</h2>
     </div>
     <div class="loading-wraper" v-if="asyncState === 'pending'">
       <div class="loader">
-        <img src="/spinner.svg" alt="">
-        <p>ipsum Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias delectus tempora totam praesentium nesciunt. Deleniti deserunt facilis commodi architecto perspiciatis nobis culpa enim voluptas possimus, harum itaque? Animi, voluptates neque.</p>
+        <img src="/spinner.svg" alt="" />
+        <p>
+          ipsum Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias
+          delectus tempora totam praesentium nesciunt. Deleniti deserunt facilis commodi
+          architecto perspiciatis nobis culpa enim voluptas possimus, harum itaque? Animi,
+          voluptates neque.
+        </p>
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
-import {postFormToDB} from '../api/'
-import FormPlanPicker from './FormPlanPicker'
-import FormUserDetails from './FormUserDetails'
-import FormAddress from './FormAddress'
-import FormReviewOrder from './FormReviewOrder'
+import { postFormToDB } from "../api/";
+import FormPlanPicker from "./FormPlanPicker";
+import FormUserDetails from "./FormUserDetails";
+import FormAddress from "./FormAddress";
+import FormReviewOrder from "./FormReviewOrder";
 export default {
-  name: 'FormWizard',
+  name: "FormWizard",
   components: {
     FormPlanPicker,
     FormUserDetails,
     FormAddress,
-    FormReviewOrder
+    FormReviewOrder,
   },
   data() {
     return {
@@ -62,66 +71,59 @@ export default {
         chocolate: false,
         otherTreat: false,
       },
-      steps: [
-        'FormPlanPicker',
-        'FormUserDetails',
-        'FormAddress',
-        'FormReviewOrder'
-      ],
-      asyncState: null
-    }
+      steps: ["FormPlanPicker", "FormUserDetails", "FormAddress", "FormReviewOrder"],
+      asyncState: null,
+    };
   },
   computed: {
-    isLastStep(){
-      return this.currentStepNumber === this.length
+    isLastStep() {
+      return this.currentStepNumber === this.length;
     },
-    wizzardInProgress(){
-      return this.currentStepNumber <= this.length
+    wizzardInProgress() {
+      return this.currentStepNumber <= this.length;
     },
     length() {
-      return this.steps.length
+      return this.steps.length;
     },
     currentStep() {
-      return this.steps[this.currentStepNumber - 1]
+      return this.steps[this.currentStepNumber - 1];
     },
     progress() {
-      return this.currentStepNumber / this.length * 100
-    }
+      return (this.currentStepNumber / this.length) * 100;
+    },
   },
   methods: {
-    updateAsyncState(state){
-      this.asyncState = state
+    updateAsyncState(state) {
+      this.asyncState = state;
     },
-    nextButtonActions(){
-      this.$refs.currentStep.submit()
-        .then(stepData => {
-          Object.assign(this.form, stepData)
+    nextButtonActions() {
+      this.$refs.currentStep
+        .submit()
+        .then((stepData) => {
+          Object.assign(this.form, stepData);
           if (this.isLastStep) {
-            this.submitOrder()
+            this.submitOrder();
           } else {
-            this.goNext()
+            this.goNext();
           }
         })
-        .catch(error => console.log(error))
+        .catch((error) => console.log(error));
     },
-    submitOrder(){
-      this.asyncState = 'pending'
-      postFormToDB(this.form)
-      .then(()=>{
-        console.log('form submited!', this.form)
-        this.asyncState = 'success'
-        this.currentStepNumber++
-      })
+    submitOrder() {
+      this.asyncState = "pending";
+      postFormToDB(this.form).then(() => {
+        console.log("form submited!", this.form);
+        this.asyncState = "success";
+        this.currentStepNumber++;
+      });
     },
-    
+
     goBack() {
-      this.currentStepNumber--
+      this.currentStepNumber--;
     },
     goNext() {
-      this.currentStepNumber++
-
-      
-    }
-  }
-}
+      this.currentStepNumber++;
+    },
+  },
+};
 </script>
